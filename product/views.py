6e1d324ebca936cdce37e183f5547357e4a9ae33
davud <aitievnurgazy@gmail.com>
2023-09-27 18:20:41 +0600

    created ProductModelViewSets

--- product/views.py
+++ product/views.py
@@ -1,8 +1,14 @@
 from rest_framework import viewsets
 
-from product.models import Category
+from product.models import Category, Product
+from product.serializers import CategorySerializer, ProductSerializer
 
 
 class CategoryModelViewSet(viewsets.ModelViewSet):
     queryset = Category.objects.all()
     serializer_class = CategorySerializer
+
+
+class ProductModelViewSet(viewsets.ModelViewSet):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
